e28267ed737106cecc82fdfc9a00e169b22f4458
arafatGithubCode <md489288@gmail.com>
2024-04-22 17:31:58 +0600

    setup express middleware

--- server/src/server.js
+++ server/src/server.js
@@ -5,14 +5,28 @@ const morgan = require("morgan");
 const app = express();
 
 app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const isLoggedIn = (req, res, next) => {
+  const login = true;
+  if (login) {
+    console.log("login success");
+    req.body.id = 101;
+    next();
+  } else {
+    return res.status(401).json({ message: "please login first" });
+  }
+};
 
 app.get("/", (req, res) => {
   res.send("Welcome to server");
 });
 
-app.get("/products", (req, res) => {
+app.get("/api/user", isLoggedIn, (req, res) => {
+  console.log(req.body.id);
   res.status(200).send({
-    message: "all products are returned",
+    message: "User profile is returned!",
   });
 });
 
