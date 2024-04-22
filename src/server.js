80ad30636a1359667839a9417e8ae602eecdefba
arafatGithubCode <md489288@gmail.com>
2024-04-22 16:17:02 +0600

    test http req & res

--- src/server.js
+++ src/server.js
@@ -2,6 +2,16 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Welcome to server");
+});
+
+app.get("/products", (req, res) => {
+  res.status(200).send({
+    message: "all products are returned",
+  });
+});
+
 app.listen(3001, () => {
   console.log("server is running at http://localhost:3001");
 });
