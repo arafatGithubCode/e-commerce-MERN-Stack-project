7701de2a07b542b930929e9bc05dacb8d495ee07
arafatGithubCode <md489288@gmail.com>
2024-04-23 10:20:49 +0600

    connect DB

--- server/src/secret.js
+++ server/src/secret.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 
 const serverPort = process.env.SERVER_PORT || 3002;
+const altasDB =
+  process.env.ATLAS_DB || "mongodb://localhost:27017/e-commerceDB";
 
-module.exports = { serverPort };
+module.exports = { serverPort, altasDB };
