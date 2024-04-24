70f9e73694c8fd905410d338221888dafa01d6af
arafatGithubCode <md489288@gmail.com>
2024-04-24 07:50:24 +0600

    update .gitignore file

--- server/src/helper/deleteImage.js
+++ server/src/helper/deleteImage.js
@@ -1,13 +1,5 @@
 const fs = require("fs").promises;
 
-// const deleteImage = (userImagePath) => {
-//   fs.access(userImagePath)
-//     .then(() => fs.unlink(userImagePath))
-//     .then(() => console.log("User image deleted successfully!"))
-//     .catch((err) => console.error("User image does not exist!"));
-// };
-
-// best practice
 const deleteImage = async (userImagePath) => {
   try {
     await fs.access(userImagePath);
