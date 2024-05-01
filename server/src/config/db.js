60f82591ea9531f7acadf613682eb4e232fda238
arafatGithubCode <md489288@gmail.com>
2024-05-01 11:43:09 +0600

    add winston logger to enhance log message

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,15 +1,17 @@
 const mongoose = require("mongoose");
+
 const { altasDB } = require("../secret");
+const logger = require("../controllers/loggerController");
 
 const connectDB = async (option = {}) => {
   try {
     await mongoose.connect(altasDB, option);
-    console.log("altasDB is connected!");
+    logger.log("info", "altasDB is connected!");
     mongoose.connection.on("error", (error) => {
-      console.error("DB connection err: ", error);
+      logger.error("error", "DB connection err: ", error);
     });
   } catch (error) {
-    console.error("Could Not connect DB: ", error.toString());
+    logger.error("error", "Could Not connect DB: ", error.toString());
   }
 };
 
